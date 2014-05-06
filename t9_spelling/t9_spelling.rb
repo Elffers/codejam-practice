b4d67a7b18efd953396eaec624376df1b30445fc
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-05-06 13:18:44 -0700

    keystroke

--- t9_spelling/t9_spelling.rb
+++ t9_spelling/t9_spelling.rb
@@ -10,6 +10,38 @@ class T9Speller
     @lines
   end
 
+  def t9(string)
 
+  end
+
+  class Keypad
+    attr_accessor :keypad
+
+    def initialize
+      @keypad = {}
+      @keypad["2"] = %w[a b c]
+      @keypad["3"] = %w[d e f]
+      @keypad["4"] = %w[g h i]
+      @keypad["5"] = %w[j k l]
+      @keypad["6"] = %w[m n o]
+      @keypad["7"] = %w[p q r s]
+      @keypad["8"] = %w[t u v]
+      @keypad["9"] = %w[w x y z]
+      @keypad["0"] = [" "]
+      @keypad
+    end
+
+    def keystroke(letter)
+      key = key_for(letter)
+      reps = @keypad[key].index(letter) + 1
+      key * reps
+    end
+
+    def key_for(letter)
+      @keypad.values.each do |array|
+        return @keypad.key(array) if array.include? letter
+      end
+    end
+  end
 
 end
--- t9_spelling/t9_spelling_spec.rb
+++ t9_spelling/t9_spelling_spec.rb
@@ -10,13 +10,13 @@ describe T9Speller do
     it 'lines should be an array' do
       expect(data.lines.first).to eq "4"
       expect(data.lines.last).to eq "hello world"
-
     end
 
     it 'should have 5 lines' do
       expect(data.lines.count).to eq 5
     end
   end
+
   context 'parse' do
     it 'returns array of strings' do
       expect(data.parse.first).to eq "hi"
@@ -28,5 +28,36 @@ describe T9Speller do
     end
   end
 
+  context 't9' do
+    string = "hi"
+    xit 'returns t9' do
+      expect(data.t9(string)).to eq "44 444"
+    end
+  end
+
+  context 'output' do
+    xit 'formats output' do
+      expect(data.output).to eq output.map {|line| line.delete("\n")}
+    end
+  end
+
+
+  context 'Keypad' do
+    let(:keypad){ T9Speller::Keypad.new }
+    context 'keystroke' do
+      it "returns keypad entry for letter" do
+        expect(keypad.keystroke("b")).to eq "22"
+        expect(keypad.keystroke("v")).to eq "888"
+        expect(keypad.keystroke(" ")).to eq "0"
+      end
+    end
+
+    context 'find_letter' do
+      it 'finds which key the letter is on' do
+        expect(keypad.key_for("a")).to eq "2"
+        expect(keypad.key_for("z")).to eq "9"
+      end
+    end
+  end
 
 end
