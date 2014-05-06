1663dcd78df697788fe8e28a9048ef63c65b8cc0
Hsing-Hui Hsu <hhhsu1@gmail.com>
2014-05-06 12:47:40 -0700

    parse

--- t9_spelling/t9_spelling.rb
+++ t9_spelling/t9_spelling.rb
@@ -1,7 +1,15 @@
 class T9Speller
   attr_accessor :lines
+
   def initialize(input)
     @lines = input.map { |line| line.delete("\n") }
   end
+
+  def parse
+    @lines.shift
+    @lines
+  end
+
+
 
 end
